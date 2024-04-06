16ed008e715154b75dd21fa844a280ec417850f1
Sajjad Hussain <sajjadast786@gmail.com>
2024-04-06 20:25:52 +0500

    14

--- app/(client)/kanban/page.jsx
+++ app/(client)/kanban/page.jsx
@@ -1,6 +1,6 @@
 // import Kanban from "./components/kanban/Kanban";
 
-import Kanban from "@/components/kanban/Kanban";
+{/*import Kanban from "@/components/kanban/Kanban";*/}
 
 export default function KanbanTest({  }) {
   return (
